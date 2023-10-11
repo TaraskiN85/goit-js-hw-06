80fb12390053818508faa483d199aab43c1f1748
TaraskiN85 <taras12304@i.ua>
2023-10-11 13:20:45 +0300

    accomplish tasks 7, 8

---
+++ js/task-07.js
@@ -0,0 +1,6 @@
+const input = document.querySelector("#font-size-control");
+const span = document.querySelector("#text");
+
+const changeFontSize = () => (span.style.fontSize = `${input.value}px`);
+
+input.addEventListener("change", changeFontSize);
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector(".login-form");
 
+const [email, password, submitButton] = [...form.elements];
+
+const checkForm = () => {
+  if (email.value === "" || password.value === "") {
+    alert("Всі поля повинні бути заповнені!");
+  }
+};
+
+const createObject = () => {
+  const userObject = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(userObject);
+};
+
+const submitForm = (e) => {
+  e.preventDefault();
+  checkForm();
+  createObject();
+};
+
+submitButton.addEventListener("click", submitForm);
